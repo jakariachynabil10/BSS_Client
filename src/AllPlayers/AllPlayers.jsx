7c4dabec60ed4147b0664d74f1421bac9ec014c0
jakariachynabil10 <jakariachynabil@gmail.com>
2023-10-16 01:54:12 +0600

    little bit update

--- src/AllPlayers/AllPlayers.jsx
+++ src/AllPlayers/AllPlayers.jsx
@@ -1,12 +1,21 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
 const AllPlayers = () => {
-    return (
-        <div>
-            <h1>All pLayers</h1>
-        </div>
-    );
+  const [players, setPlayers] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/allPlayers")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setPlayers(data);
+      });
+  }, []);
+  return (
+    <div>
+      <h1>All pLayers</h1>
+    </div>
+  );
 };
 
 export default AllPlayers;
